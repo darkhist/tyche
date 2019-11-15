4e9ece0f51e114424893931d810df439a0d730dd
darkhist <qmsalas321@gmail.com>
2019-11-15 11:34:35 -0600

    wip: url generation add random number generator

--- src/components/generator.jsx
+++ src/components/generator.jsx
@@ -1,3 +1,5 @@
+const generateRandomNumber = () => Math.floor(Math.random() * 250000) + 1;
+
 const generateURL = () => 'https://archillect.com/123';
 
-export default generateURL;
+export { generateRandomNumber, generateURL };
--- src/tests/generator.test.jsx
+++ src/tests/generator.test.jsx
@@ -1,6 +1,15 @@
-import generateURL from '../components/generator';
+import {
+  generateRandomNumber,
+  generateURL
+} from '../components/generator';
 
 describe('Generator', () => {
+  it('should generate a random number between 1 and 250,000', () => {
+    const random = generateRandomNumber();
+    expect(random).toBeGreaterThan(0);
+    expect(random).toBeLessThan(250000);
+  });
+
   it('should generate a random url', () => {
     const url = generateURL();
     expect(url).toContain('https://archillect.com/');
