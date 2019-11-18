663620534c643cea39a4c1af0c06fc0e1a350a2c
darkhist <qmsalas321@gmail.com>
2019-11-18 15:36:33 -0600

    fix: resolve cors issue prepend url with cors proxy url

--- src/components/generator.jsx
+++ src/components/generator.jsx
@@ -1,6 +1,8 @@
 const generateRandomNumber = () => Math.floor(Math.random() * 250000) + 1;
 
-const generateURL = () =>
-  `https://archillect.com/${generateRandomNumber()}`;
+const generateURL = () => {
+  const postNo = generateRandomNumber();
+  return `${process.env.PROXY_URL}https://archillect.com/${postNo}`;
+};
 
 export { generateRandomNumber, generateURL };
--- src/tests/generator.test.jsx
+++ src/tests/generator.test.jsx
@@ -3,7 +3,7 @@ import { generateURL } from '../components/generator';
 describe('Generator', () => {
   it('should generate a random url', () => {
     const url = generateURL();
-    const num = Number.parseInt(url.slice(23), 10);
+    const num = Number.parseInt(url.slice(url.lastIndexOf('/') + 1), 10);
     expect(url).toContain('https://archillect.com/');
     expect(num).toBeGreaterThan(0);
     expect(num).toBeLessThan(250000);
